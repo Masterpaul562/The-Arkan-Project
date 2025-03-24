075e48a649f4807e37ef9d37737365b5cd67a959
Masterpaul562 <9652749@graniteschools.org>
2025-03-23 20:57:04 -0600

    firing meh

--- Assets/Scripts/Enemies/Move_Heavy.cs
+++ Assets/Scripts/Enemies/Move_Heavy.cs
@@ -13,11 +13,31 @@ public class Move_Heavy : MonoBehaviour
     private int health;
     public float angle;
     private bool shouldShoot;
+    [SerializeField] private GameObject bullet;
+    [SerializeField, Range(0, 30)] private float fireRate;
+    [SerializeField, Range(0, 30)] private float bulletSpeed;
+    [SerializeField] private Transform bulletSpawn;
+    private bool canFire;
+    Rigidbody rb;
 
     void Start()
     {
         currentLoc = jumpLoc[0];
         StartCoroutine(FOVRoutine());
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        StartCoroutine(FireRateHandler());
+
+    }
+    private IEnumerator FireRateHandler()
+    {
+        float timeToNextFire = fireRate;
+        WaitForSeconds wait = new WaitForSeconds(timeToNextFire);
+        while (true)
+        {
+            yield return timeToNextFire;
+            canFire = true;
+        }
     }
     private IEnumerator FOVRoutine()
     {
@@ -47,7 +67,16 @@ public class Move_Heavy : MonoBehaviour
         Statehandler();
         if (state == MovementState.shooting)
         {
-
+            float? angle = RotateSpawn();
+            if (angle != null)
+            {
+                if (canFire)
+                {
+                    canFire = false;
+                    Fire();
+                }
+            }
+            
         } else if (state == MovementState.rotating)
         {
             target = player;
@@ -97,4 +126,44 @@ public class Move_Heavy : MonoBehaviour
             shouldShoot = false;   
         }
     }
+    float? RotateSpawn()
+    {
+        float? angle = CalculateAngle(true);
+
+        if (angle != null)
+        {
+
+            bulletSpawn.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
+        }
+        return angle;
+    }
+    float? CalculateAngle(bool low)
+    {
+        Vector3 targetDir = target.transform.position - bulletSpawn.position;
+        float y = targetDir.y;
+        targetDir.y = 0f;
+        float x = targetDir.magnitude;
+        float gravity = 9.8f;
+        float sSqr = bulletSpeed * bulletSpeed;
+        float underSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+
+        if (underSqrRoot >= 0f)
+        {
+            float root = Mathf.Sqrt(underSqrRoot);
+            float highAngle = sSqr * root;
+            float lowAngle = sSqr - root;
+            if (low)
+                return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+            else
+                return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+        }
+        else
+            return null;
+    }
+    private void Fire()
+    {
+        GameObject projectile = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+        projectile.GetComponent<Rigidbody>().velocity = bulletSpeed * bulletSpawn.forward;
+    }
+    
 }
